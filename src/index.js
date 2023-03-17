e76c35b5552a4893472050ff7667a48857967cd8
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-03-17 16:27:51 +0200

    add BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
-import { theme } from "./theme";
-
+import { theme } from './theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter basename="/petly-front">
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
-
   </React.StrictMode>
 );
