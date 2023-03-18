41bd2e5ecf85e2565f332007472f7a46911b4349
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-03-19 01:55:35 +0200

    add wrapper

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,13 +2,15 @@ import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header';
 import { Suspense } from 'react';
 
+import { Wrapper } from './SharedLayout.styled';
+
 export const SharedLayout = () => {
   return (
-    <>
+    <Wrapper>
       <Header />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-    </>
+    </Wrapper>
   );
 };
