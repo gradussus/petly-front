c143b0a43821b283bc62832c93295bfeaaf47e57
Aleksandr Cheverda <alexar@ua.fm>
2023-03-18 16:30:49 +0200

    upd file structure

--- src/components/Container.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import scss from './Container.module.scss';
-
-function Container({ children }) {
-  return <div className={scss.container}>{children}</div>;
-}
-
-export default Container;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './components/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
