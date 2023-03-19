029e410d04f3419d50a6ec160d57846125b70fd1
Aleksandr Cheverda <alexar@ua.fm>
2023-03-19 21:50:50 +0200

    add elements HomePage
    Додано елементи сторінки Home без стилізації

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,28 @@
-import { HomePageContainer } from './HomePage.styled';
-import { HomePage } from 'pages/HomePage/HomePage';
-import HomeLabro from '../../pages/HomePage/images/mobile/labro.png';
-import HomeHeart from '../../pages/HomePage/images/desktop/heart.png';
-import { Wrapper } from './SharedLayout.styled';
+// import { HomePageContainer } from './HomePage.styled';
+// import { HomePage } from 'pages/HomePage/HomePage';
+// import HomeLabro from '../../pages/HomePage/images/mobile/labro.png';
+// import HomeHeart from '../../pages/HomePage/images/desktop/heart.png';
+// import { Wrapper } from './SharedLayout.styled';
 
-export const HomePage = ({ HomeHeart, HomeLabro }) => {
+// export const HomePage = () => {
+//   return <home>Hello this is Home</home>;
+// };
 
-    return (
-        <Wrapper>
-        <HomePageContainer>
-            <h1>
-                <span>Take good care of</span>
-                  <br />
-                <span>your small pets</span>
-            </h1>
-            <img src={HomeHeart} alt={HomeHeart} />
-            <img src={HomeLabro} alt={HomeLabro} />
-            {/* <HomeLabro />
-            <HomeHeart /> */}
-            </HomePageContainer>
-            </Wrapper>
-    );
-  };
+// export const HomePage = () => {
+
+//     return (
+//         <Wrapper>
+//         <HomePageContainer>
+//             <h1>
+//                 <span>Take good care of</span>
+//                   <br />
+//                 <span>your small pets</span>
+//             </h1>
+//             <img src={HomeHeart} alt={HomeHeart} />
+//             <img src={HomeLabro} alt={HomeLabro} />
+//             {/* <HomeLabro />
+//             <HomeHeart /> */}
+//             </HomePageContainer>
+//             </Wrapper>
+//     );
+//   };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <p>HomePage</p>;
+import heartImg from './images/desktop/heart.png';
+import labro from './images/mobile/labro.png';
+import vector from './images/mobile/Vector.png';
+
+const HomePage = ({heart, labrador, background}) => {
+  return <home>
+    <span>Take good care of</span>
+    <br />
+    <span>your small pets</span>
+    <img src={heartImg} alt="heart" />
+    <img src={labro} alt="labrador" />
+    <img src={vector} alt="background" />
+  </home>;
 };
 
 export default HomePage;
