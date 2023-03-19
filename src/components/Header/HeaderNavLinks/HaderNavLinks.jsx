d1a76f14289ee011e116d05f38a0eeb0e168d46c
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-03-19 22:26:40 +0200

    prettier

--- src/components/Header/HeaderNavLinks/HaderNavLinks.jsx
+++ src/components/Header/HeaderNavLinks/HaderNavLinks.jsx
@@ -1,15 +1,15 @@
 import * as LinksStyle from './HeaderNavLinks.styled';
+// мокап пока нема з беку цього
 import NavMenu from '../Data/NavMenu.json';
 
-console.log(NavMenu);
-
 export const HeaderNavLinks = () => {
-  return ( <>
-  {NavMenu.map(item => (<LinksStyle.ItemLink key={item.href} to={item.href}>{item.title}</LinksStyle.ItemLink>))}
-
-
-  </>
- 
+  return (
+    <>
+      {NavMenu.map(item => (
+        <LinksStyle.ItemLink key={item.href} to={item.href}>
+          {item.title}
+        </LinksStyle.ItemLink>
+      ))}
+    </>
   );
 };
-
