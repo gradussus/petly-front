6805f5c7edb71d26fdf1d831f4dc63368f8e1e89
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2023-03-19 13:12:19 +0200

    add fonts add header styles

--- src/components/App.Style.js
+++ src/components/App.Style.js
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 export const Title = styled.div`
 height: 100vh;
-display: flex;
-justify-content: center;
-align-items: center;
 font-size: ${p => p.theme.fontSizes.lx}px;
 font-weight: ${p => p.theme.fontWeights.bold};
 color: ${p => p.theme.colors.primeText};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import { Title } from "./App.Style";
+// import { Title } from "./App.Style";
 import { lazy } from 'react';
 
-const LoginPage = lazy(() => import('../pages/Login/Login'));
+// const LoginPage = lazy(() => import('../pages/Login/Login'));
+// const Header = lazy(() => import('../components/Header/Header'));
+import Header from './Header/Header';
+
 
 export const App = () => {
   
   return (
-     <Title>
-      <LoginPage/>
-    </Title>
+    <Header/>
+    
+    //  <Title>
+      
+    //   {/* <LoginPage/> */}
+    // </Title>
   );
 };
