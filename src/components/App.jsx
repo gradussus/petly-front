212bf81794ecb2508e49ce07583974413254ca6c
andrii-pozniak <andriipozniak@gmail.com>
2023-03-19 11:54:33 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Title } from "./App.Style";
 import { lazy } from 'react';
-import { FoundPets } from "components/FundPets/FoundPets";
 const LoginPage = lazy(() => import('../pages/Login/Login'));
 
 export const App = () => {
@@ -10,7 +9,6 @@ export const App = () => {
   return (
      <Title>
       <LoginPage/>
-      <FoundPets/>
     </Title>
   );
 };
