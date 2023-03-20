5b07481e6bcf015376209ce2336573b6cc5819df
Aleksandr Cheverda <alexar@ua.fm>
2023-03-20 22:50:40 +0200

    upd files

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,21 @@
 import heartImg from './images/desktop/heart.png';
 import labro from './images/mobile/labro.png';
 import vector from './images/mobile/Vector.png';
+import { Container } from './Container/Container';
+import styles from "../HomePage/HomePage.module.css";
 
 const HomePage = ({heart, labrador, background}) => {
   return <home>
-    <span>Take good care of</span>
-    <br />
-    <span>your small pets</span>
-    <img src={heartImg} alt="heart" />
-    <img src={labro} alt="labrador" />
-    <img src={vector} alt="background" />
+    <Container className={styles.container}>
+      <div className={styles.home_title}>
+        <span>Take good care of</span>
+        <br />
+        <span>your small pets</span>
+      </div>
+    <img className={styles.img_heart} src={heartImg} alt="heart" />
+      <img className={styles.img_labro} src={labro} alt="labrador" />
+    <img className={styles.img_vector} src={vector} alt="background" />
+    </Container>
   </home>;
 };
 
