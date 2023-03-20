780b4eea3a8000a1ffe2d8937acad77e6ce004b8
Aleksandr Cheverda <alexar@ua.fm>
2023-03-21 00:19:02 +0200

    upd styles home

--- src/pages/HomePage/Container/Container.jsx
+++
@@ -1,5 +0,0 @@
-// import styles from '../Container/Container.module.css';
-
-export const Container = ({ children }) => {
-    return <div className='styles.container'>{children}</div>
-}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 import heartImg from './images/desktop/heart.png';
-import labro from './images/mobile/labro.png';
-import vector from './images/mobile/Vector.png';
-import { Container } from './Container/Container';
+import labro from './images/mobile/labro_mobile.png';
+import vector from './images/mobile/bg_mobile.png';
+// import { Container } from './Container/Container';
 import styles from "../HomePage/HomePage.module.css";
 
 const HomePage = ({heart, labrador, background}) => {
-  return <home>
-    <Container className={styles.container}>
+  return <home className={styles.container}>
+    {/* <Container className={styles.container}> */}
       <div className={styles.home_title}>
         <span>Take good care of</span>
         <br />
@@ -15,7 +15,7 @@ const HomePage = ({heart, labrador, background}) => {
     <img className={styles.img_heart} src={heartImg} alt="heart" />
       <img className={styles.img_labro} src={labro} alt="labrador" />
     <img className={styles.img_vector} src={vector} alt="background" />
-    </Container>
+    {/* </Container> */}
   </home>;
 };
 
