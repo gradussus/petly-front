9595ea48814099029457e260092e0f1bbaa9dc44
gradussus <103496948+gradussus@users.noreply.github.com>
2023-03-21 21:22:50 +0000

    Deploying to gh-pages from @ gradussus/petly-front@f980ee3931d172a087ac64392cf4cd3bddeec062 🚀

--- static/js/35.7287181c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpetly_front=self.webpackChunkpetly_front||[]).push([[35],{2035:function(e,n,t){t.r(n),t.d(n,{default:function(){return u}});var r=t(184),u=function(){return(0,r.jsx)("p",{children:"HomePage"})}}}]);
-//# sourceMappingURL=35.7287181c.chunk.js.map
