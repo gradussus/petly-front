0039d7cdb8874a7dd49ae971581e03690a049553
andrii-pozniak <andriipozniak@gmail.com>
2023-03-23 14:52:05 +0200

    add pull fron main

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,28 +1,4 @@
 // import { useSelector } from 'react-redux';
-<<<<<<< HEAD
-import NoticeCategoryItem from "components/NoticesPage/NoticeCategoryItem/NoticeCategoryItem";
-import axios from "axios";
-import AddPets from "components/NoticesPage/AddPets";
-import { Ul, ContainerCard } from "../NoticesPage/NoticesPage.Style";
-import { ModalSample } from "components/Modal/Modal";
-import { ItemPetModal } from "components/ItemPetModal/ItemPetModal";
-import { useEffect, useState } from "react";
-
-const NAME_URL = 'https://petly-vxdt.onrender.com/notices/'
-
-const NoticesCategoriesList = ({ type, foundPets }) => {
-  const [states, setStates] = useState([])
-  // const [pets, setPets] = useState('notices')
-  const [showModal, setShowModal] = useState(false);
-  const [noticeId, setNoticeId] = useState('')
-  const [modalCard, setModalCard] = useState([])
-  const [query, setQuery] = useState('')
-  // const query =
-  const handleChange = (id) => setNoticeId(id)
-
-  const {namePets} = foundPets
-  console.log("first", foundPets)
-=======
 import NoticeCategoryItem from 'components/NoticesPage/NoticeCategoryItem/NoticeCategoryItem';
 import axios from 'axios';
 import { Ul, ContainerCard } from '../NoticesPage/NoticesPage.Style';
@@ -43,40 +19,9 @@ const NoticesCategoriesList = ({ type }) => {
   // const query =
   const handleChange = id => setNoticeId(id);
 
->>>>>>> d731e21b55f7d0eff102b3934c5de0b724c817ac
   useEffect(() => {
     
     async function fetchNotice() {
-<<<<<<< HEAD
-      try {    
-        console.log('first',`${NAME_URL}${query}`)    
-        const { data } = await axios.get(`${NAME_URL}${query}`)        
-        setStates(data)
-
-      } catch (error) {
-        console.log({message: error})
-      } finally {
-        
-      }
-    }
-    fetchNotice()
-  }, [ type, query])
-// console.log("states", states)
-  useEffect(() => {
-
-    if (!namePets) {
-      return setQuery(`${type}`)
-    }
-    
-    setQuery(`search/${type}/${namePets}`)
-    // console.log('first',`${NAME_URL}${query}`)
-    
-
-    
-  }, [namePets, type, query])
-
-  // console.log('query', query)
-=======
       try {
         if (['sell', 'lost-found', 'for-free'].includes(type)) {
           const { data } = await axios.get(`${NAME_URL}notices/${type}`);
@@ -93,7 +38,6 @@ const NoticesCategoriesList = ({ type }) => {
     }
     fetchNotice();
   }, [type, token]);
->>>>>>> d731e21b55f7d0eff102b3934c5de0b724c817ac
 
   useEffect(() => {
     if (!noticeId) {
