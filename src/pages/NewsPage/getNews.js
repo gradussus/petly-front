e30dff9656db543c5d0a83f51eec964a9a3132d2
Maks Novakov <maksnovakov78@gmail.com>
2023-03-24 11:32:51 +0200

    add useEffect

--- src/pages/NewsPage/getNews.js
+++ src/pages/NewsPage/getNews.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
 
 axios.defaults.baseURL = 'https://petly-vxdt.onrender.com';
 
-const getNews = async () => {
+export const getNews = async () => {
   try {
     const response = await axios.get('/news');
     return response.data;
@@ -11,4 +13,28 @@ const getNews = async () => {
   }
 };
 
-export default getNews;
+// const NewsData = () => {
+//   const [data, setData] = useState(null);
+
+//   const [status, setStatus] = useState('pending');
+
+//   useEffect = () => {
+//     (async () => {
+//       try {
+//         setStatus('pending');
+
+//         const data = await getNews();
+//         setStatus('fulfilled');
+//         setData(data);
+//       } catch {
+//         setStatus('rejected');
+//       }
+//     })();
+//   };
+// };
+
+// useEffect = () => {
+//   if ('rejected') {
+//     toast.error('Nothing found. Please, try again.', { theme: 'colored' });
+//   }
+// };
