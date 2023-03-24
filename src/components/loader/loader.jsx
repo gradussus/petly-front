4ea3b611040bb1f4fceeea5467afd5d949dc4473
MykolaPom <mykola.pomazanov@gmail.com>
2023-03-24 12:02:16 +0200

    styles reset on Loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -3,18 +3,7 @@ import React from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
 
 const Loader = ({ size = 100 }) => {
-  const style = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-  };
-
-  return (
-    <div style={style}>
-      <ClipLoader size={size} color="#000" />
-    </div>
-  )
+  return <ClipLoader size={size} color="#000" />;
 };
 
 export default Loader;
