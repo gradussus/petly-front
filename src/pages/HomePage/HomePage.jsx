3c437b978452831199550324210824fbcf40b893
Aleksandr Cheverda <alexar@ua.fm>
2023-03-26 11:31:13 +0300

    fix HomePage
    Скорочено час анімації, відкореговано положення Лабрадора

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,9 @@ import * as SC from './HomePage.styled.js';
 
 import heartImg from './images/desktop/heart.png';
 import labro from './images/desktop/labro_desk.png';
-// import labro from './labro_524_d_1x.png';
 import vectorL from './images/desktop/bg_desk_left.png';
 import vectorR from './images/desktop/bg_desk_right.png';
 import vectorSm from './images/tablet/bg_tablet.png';
-
 
 const HomePage = () => {
   return <SC.Home>
@@ -16,12 +14,6 @@ const HomePage = () => {
         <span>your small pets</span>
       </SC.Title>
     <SC.Heart src={heartImg} alt="heart" />
-    
-      {/* <source media='(max-width: 767px)' srcSet='labro_280_m_1x.png' />
-      <source media='(min-width: 768px) and (max-width: 1279px)' srcset='labro_636_t_1x.png' />
-      <source media='(min-width: 1280px)' srcSet='labro_524_d_1x.png' /> */}
-      {/* <SC.Labrador src={labro} alt='Labrador'/> */}
-    
     <SC.Labro src={labro} alt="labrador" />
     <SC.BackgroundL src={vectorL} alt="background" /> 
     <SC.BackgroundR src={vectorR} alt="background" />
