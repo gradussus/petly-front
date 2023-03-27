8c64db579432a74a12fd56b25bce5c76e6078ba8
andrii-pozniak <andriipozniak@gmail.com>
2023-03-27 10:21:39 +0300

    change indents in modal

--- src/components/Notices/ItemPetModal/ItemPetModal.Style.js
+++ src/components/Notices/ItemPetModal/ItemPetModal.Style.js
@@ -18,7 +18,7 @@ export const Container = styled.div`
 
 export const Modal = styled.div`
   position: absolute;
-  bottom: 50%;
+  top: 50%;
   left: 50%;
   transform: translate(-50% -50%);
   padding: 20px;
