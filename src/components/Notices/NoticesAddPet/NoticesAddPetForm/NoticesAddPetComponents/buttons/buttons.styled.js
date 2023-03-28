f93f510a5a64e727df44f1416fea7834fd8e9fff
Daria Sliusar <darinacat87@gmail.com>
2023-03-28 22:11:56 +0300

    Updated styles for modal add pet

--- src/components/Notices/NoticesAddPet/NoticesAddPetForm/NoticesAddPetComponents/buttons/buttons.styled.js
+++ src/components/Notices/NoticesAddPet/NoticesAddPetForm/NoticesAddPetComponents/buttons/buttons.styled.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { theme } from '../../../../../../theme'
+import { theme } from '../../../../../../theme';
 
 export const TypeofAddBtn = styled.button`
   padding: 10px 28px;
@@ -12,47 +12,25 @@ export const TypeofAddBtn = styled.button`
   color: #111111;
   transition: all 0.3s;
 
-  margin-right: auto;
+  margin-left: 12px;
+  margin-right: 12px;
+  margin-top: 12px;
+  float: left;
+  cursor: pointer;
 
   :hover,
   &.active {
     background: #f59256;
     color: #fff;
-  }`;
-export const TypeofAddBtnActive = styled(TypeofAddBtn)`
-  background:  ${theme.colors.accent};
-    color:  ${theme.colors.white};
+  }
+`;
 
-`
+export const TypeofAddBtnActive = styled(TypeofAddBtn)`
+  background: ${theme.colors.accent};
+  color: ${theme.colors.white};
+`;
 
 export const DoneBtn = styled.button`
-
-padding: 10px 28px;
-  background: #ffffff;
-  border: 2px solid #f59256;
-  border-radius: 40px;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 27px;
-  transition: all 0.3s;
-
-  margin-right: 20px;
-  width: 180px;
-  color: ${theme.colors.white};
-  background-color: ${theme.colors.accent};
-:hover,
-  &.active {
-    background:${theme.colors.white};
-    color: ${theme.colors.black};
-  }
-`
-
-export const CancelBtn = styled(TypeofAddBtn)`
-margin-right: 20px;
-width: 180px;
-`
-export const NextBtn = styled.div`
-
   padding: 10px 28px;
   background: #ffffff;
   border: 2px solid #f59256;
@@ -66,34 +44,58 @@ export const NextBtn = styled.div`
   width: 180px;
   color: ${theme.colors.white};
   background-color: ${theme.colors.accent};
-:hover,
+  cursor: pointer;
+  :hover,
   &.active {
-    background: ${theme.colors.white};
-    color: ${theme.colors.black};
+    background: ${theme.colors.backgroundSecondary};
   }
-`
+`;
+
+export const CancelBtn = styled(TypeofAddBtn)`
+  margin-right: 20px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  width: 180px;
+`;
+export const NextBtn = styled.div`
+  padding: 10px 28px;
+  background: #ffffff;
+  border: 2px solid #f59256;
+  border-radius: 40px;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+  transition: all 0.3s;
+
+  margin-right: 20px;
+  width: 180px;
+  color: ${theme.colors.white};
+  background-color: ${theme.colors.accent};
+  cursor: pointer;
+  :hover,
+  &.active {
+    background: ${theme.colors.backgroundSecondary};
+  }
+`;
 
 export const Wrapper = styled.div`
-display:flex;
-max-width: 380px;
-`
+  display: flex;
+  max-width: 380px;
+`;
 
 export const TypeOfSexBtn = styled.button`
-width:100%;
-background-color: transparent;
-color: ${theme.colors.black};
-border: none;
-:hover,
+  width: 100%;
+  background-color: transparent;
+  color: ${theme.colors.black};
+  border: none;
+  :hover,
   &.active {
     color: ${theme.colors.accent};
   }
-`
+`;
 export const ActiveTypeOfSexBtn = styled.button`
-width:100%;
-background-color: transparent;
-color: ${theme.colors.accent};
-border: none;
-
-
-`
-
+  width: 100%;
+  background-color: transparent;
+  color: ${theme.colors.accent};
+  border: none;
+`;
--- src/components/Notices/NoticesAddPet/NoticesAddPetForm/NoticesAddPetComponents/inputs/input.styled.js
+++ src/components/Notices/NoticesAddPet/NoticesAddPetForm/NoticesAddPetComponents/inputs/input.styled.js
@@ -1,27 +1,26 @@
 import styled from 'styled-components';
 import { Field } from 'formik';
-import { theme } from '../../../../../../theme'
+import { theme } from '../../../../../../theme';
 
 export const FileInput = styled.input`
-opacity: 0;
- visibility: hidden;
-/* background-color: red;
+  opacity: 0;
+  visibility: hidden;
+  /* background-color: red;
 width: 100%;
 height: 100% */
-`
+`;
 export const LabelFileInput = styled.label`
-width: 140px;
-height: 140px;
-background: #FDF7F2;
-border-radius: 20px;
-margin-right: auto;
-`
-export const CommentsInput= styled(Field)`
-
+  width: 140px;
+  height: 140px;
+  background: #fdf7f2;
+  border-radius: 20px;
+  margin-right: auto;
+`;
+export const CommentsInput = styled(Field)`
   width: 100%;
   box-sizing: border-box;
-  margin-left:auto;
-  margin-right:auto;
+  margin-left: auto;
+  margin-right: auto;
   border-radius: ${theme.radii.large};
   margin-bottom: 40px;
   padding: 12px 32px;
@@ -29,9 +28,54 @@ export const CommentsInput= styled(Field)`
   background: ${theme.colors.background};
   color: ${theme.colors.primeText};
 
-  @media screen and (min-width: 768px){
+  @media screen and (min-width: 768px) {
     /* min-height: 113px;
     width: 448px; */
-}
+  }
+`;
+export const FormAddPet = styled.div`
+  width: 450px;
+  margin-bottom: 28px;
+  position: relative;
+  @media (max-width: 767px) {
+    width: 300px;
+  }
+`;
 
-`
+export const LabelTitle = styled.label`
+  font-family: ${p => p.theme.fonts.manrope};
+  font-weight: ${p => p.theme.fontWeights.middle};
+  font-size: ${p => p.theme.fontSizes.ml};
+  line-height: 26px;
+  margin-bottom: 12px;
+  float: left;
+`;
+
+export const AddPetFormInput = styled(Field)`
+  width: 100%;
+  background: ${p => p.theme.colors.background};
+  border: 1px solid ${p => p.theme.colors.accent};
+  outline: none;
+  border-radius: ${p => p.theme.radii.large};
+  padding: 10px 16px;
+  font-weight: ${p => p.theme.fontWeights.normal};
+  font-size: ${p => p.theme.fontSizes.m};
+  line-height: 26px;
+  cursor: pointer;
+  &:focus-within,
+  &:hover {
+    box-shadow: 0 0 0 1px ${p => p.theme.colors.accent};
+    @media screen and (min-width: 768px) {
+      box-shadow: 0 0 0 1px ${p => p.theme.colors.accent};
+    }
+  }
+`;
+
+export const TypeOfAddNav = styled.div`
+  margin: -12px;
+  margin-bottom: 28px;
+`;
+
+export const Mandatory = styled.span`
+  color: ${p => p.theme.colors.activeAccent};
+`;
